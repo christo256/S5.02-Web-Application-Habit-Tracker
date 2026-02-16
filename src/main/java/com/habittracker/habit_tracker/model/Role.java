8aaa90c71adecc18d6fc1eca1fcac8193ac6278f
Christopher <ckingmaster2@gmail.com>
2026-02-16 11:06:10 +0100

    Making the first commit to add my project to git

---
+++ src/main/java/com/habittracker/habit_tracker/model/Role.java
@@ -0,0 +1,6 @@
+package com.habittracker.habit_tracker.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/habittracker/habit_tracker/model/User.java
@@ -0,0 +1,4 @@
+package com.habittracker.habit_tracker.model;
+
+public class User {
+}
