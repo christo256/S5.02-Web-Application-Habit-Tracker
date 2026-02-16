12b18be27b5fec351de04276bcb75b78e4946968
Christopher <ckingmaster2@gmail.com>
2026-02-16 13:33:35 +0100

    feat(model): add User entity with JPA mappings

--- src/main/java/com/habittracker/habit_tracker/model/User.java
+++ src/main/java/com/habittracker/habit_tracker/model/User.java
@@ -1,4 +1,35 @@
 package com.habittracker.habit_tracker.model;
 
+import com.habittracker.habit_tracker.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Habit> habits = new ArrayList<>();
 }
