4d1b23a6f236203c3e197104c85ec60462eb90ca
christo256 <ckingmaster2@gmail.com>
2026-02-23 12:18:27 +0100

    feat(dto): add JwtResponse for authentication

---
+++ src/main/java/com/habittracker/habit_tracker/dto/response/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.habittracker.habit_tracker.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+
+}
